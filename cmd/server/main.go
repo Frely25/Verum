4d76074cd59ec61e204fa5930b976f562efd5718
Frely25 <okefrely@gmail.com>
2026-09-01 02:03:46 +0300

    Связал новую структуру

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,19 +7,26 @@ import (
 )
 
 func main() {
+	// var repo class.Repository
+	// var service class.Service
+	// var handler class.Handler
+
+	repo, err := class.NewMemoryRepository()
+	if err != nil {
+		panic(err)
+	}
+
+	handler := class.NewHandler(repo)
+
 	mux := http.NewServeMux()
 
-	// var repo class.Repository
-
-	// repo, err := class.NewMemoryRepository()
-
-	mux.HandleFunc("/health", class.HealthsHandler)
-	mux.HandleFunc("/classes", class.ClassesHandler)
-	mux.HandleFunc("GET /classes/{id}", class.GetClassByID)
+	mux.HandleFunc("/health", handler.HealthHandler)
+	mux.HandleFunc("/classes", handler.ClassesHandler)
+	mux.HandleFunc("GET /classes/{id}", handler.GetClassByID)
 
 	log.Println("server started on http://localhost:8080")
 
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
