dea20a61d3971fc0a8f082e2d4d4e2592e2c55f1
Frely25 <okefrely@gmail.com>
2026-09-13 21:55:38 +0300

    Добавил интерфейс для repository

---
+++ internal/features/user/service/repository.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Frely25/Verum/internal/core/domains"
+)
+
+type Repository interface {
+	Create(ctx context.Context, user domains.User) (domains.User, error)
+	GetByID(ctx context.Context, id int) (domains.User, error)
+	GetByLogin(ctx context.Context, login string) (domains.User, error)
+}
