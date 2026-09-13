d8423a9131ca911abe20e931883ed32cf8ec317c
Frely25 <okefrely@gmail.com>
2026-09-13 22:11:10 +0300

    Добавил в Service Create

---
+++ internal/features/user/service/service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"strings"
+
+	"github.com/Frely25/Verum/internal/core/domains"
+	apperrors "github.com/Frely25/Verum/internal/core/errors"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
+
+func (s *Service) Create(ctx context.Context, newUser domains.User) (domains.User, error) {
+	newUser.Login = strings.TrimSpace(newUser.Login)
+	newUser.DisplayName = strings.TrimSpace(newUser.DisplayName)
+
+	if newUser.Login == "" ||
+		newUser.DisplayName == "" ||
+		newUser.PasswordHash == "" {
+
+		return domains.User{}, apperrors.ErrInvalidUser
+	}
+
+	return s.repo.Create(ctx, newUser)
+}
