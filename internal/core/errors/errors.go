31312cfc3645d92e530e6422ba216fb3a623dd57
Frely25 <okefrely@gmail.com>
2026-09-13 21:50:06 +0300

    Добавил ошибки для user feature

--- internal/core/errors/errors.go
+++ internal/core/errors/errors.go
@@ -3,6 +3,12 @@ package apperrors
 import "errors"
 
 var (
+	// Class errors
 	ErrClassNotFound    = errors.New("class not found")
 	ErrInvalidClassName = errors.New("invalid class name")
+
+	// User errors
+	ErrUserNotFound      = errors.New("user not found")
+	ErrLoginAlreadyTaken = errors.New("login already taken")
+	ErrInvalidUser       = errors.New("invalid user")
 )
