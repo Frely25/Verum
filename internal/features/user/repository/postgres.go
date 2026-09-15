85e7c8aeb59e615d6b6bf91378bc29bcfa8dd1aa
Frely25 <okefrely@gmail.com>
2026-09-15 18:39:14 +0300

    Добавил user GetByID и добавил папки для auth

--- internal/features/user/repository/postgres.go
+++ internal/features/user/repository/postgres.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Frely25/Verum/internal/core/domains"
 	apperrors "github.com/Frely25/Verum/internal/core/errors"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -44,6 +45,42 @@ func (r *PostgreRepository) Create(ctx context.Context, newUser domains.User) (d
 		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
 			return domains.User{}, apperrors.ErrLoginAlreadyTaken
 		}
+		return domains.User{}, err
+	}
+
+	return model.toDomain(), nil
+}
+
+func (r *PostgreRepository) GetByID(ctx context.Context, id int) (domains.User, error) {
+	query := `
+		SELECT
+			id,
+			login,
+			password_hash,
+			display_name,
+			created_at,
+			updated_at
+		FROM users
+		WHERE id = $1
+	`
+
+	var model userModel
+
+	err := r.pool.QueryRow(ctx, query, id).Scan(
+		&model.ID,
+		&model.Login,
+		&model.PasswordHash,
+		&model.DisplayName,
+		&model.CreatedAt,
+		&model.UpdatedAt,
+	)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return domains.User{},
+			apperrors.ErrUserNotFound
+	}
+
+	if err != nil {
 		return domains.User{}, err
 	}
 
