9dc38272af033a301b46ab4135699c1411d01b61
Frely25 <okefrely@gmail.com>
2026-09-15 18:48:35 +0300

    Добавил errors для auth

--- internal/core/errors/errors.go
+++ internal/core/errors/errors.go
@@ -11,4 +11,10 @@ var (
 	ErrUserNotFound      = errors.New("user not found")
 	ErrLoginAlreadyTaken = errors.New("login already taken")
 	ErrInvalidUser       = errors.New("invalid user")
+
+	// Auth errors
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUnauthorized       = errors.New("unauthorized")
+	ErrSessionNotFound    = errors.New("session not found")
+	ErrInvalidAuthInput   = errors.New("invalid auth input")
 )
