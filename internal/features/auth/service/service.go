674e50276bfe48fe87c934a0b9c84637d2a5d8e1
Frely25 <okefrely@gmail.com>
2026-09-17 15:49:54 +0300

    ...

--- internal/features/auth/service/service.go
+++ internal/features/auth/service/service.go
@@ -1,1 +1,51 @@
 package service
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/Frely25/Verum/internal/core/domains"
+	apperrors "github.com/Frely25/Verum/internal/core/errors"
+	"github.com/Frely25/Verum/internal/features/auth/transport"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const sessionTTL = 24 * time.Hour
+
+type Service struct {
+	users    UserService
+	sessions SessionRepository
+}
+
+func NewService(users UserService, sessions SessionRepository) *Service {
+	return &Service{
+		users:    users,
+		sessions: sessions,
+	}
+}
+
+func (s *Service) Register(ctx context.Context, req transport.RegisterRequest) (domains.User, error) {
+
+	req.Login = strings.TrimSpace(req.Login)
+	req.DisplayName = strings.TrimSpace(req.DisplayName)
+
+	if req.Login == "" || req.Password == "" || req.DisplayName == "" {
+		return domains.User{},
+			apperrors.ErrInvalidAuthInput
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return domains.User{}, err
+	}
+
+	newUser := domains.User{
+		Login:        req.Login,
+		PasswordHash: string(passwordHash),
+		DisplayName:  req.DisplayName,
+	}
+
+	return s.users.Create(ctx, newUser)
+}
--- internal/features/auth/transport/dto.go
+++ internal/features/auth/transport/dto.go
@@ -1,1 +1,29 @@
 package transport
+
+import "github.com/Frely25/Verum/internal/core/domains"
+
+type RegisterRequest struct {
+	Login       string `json:"login"`
+	Password    string `json:"password"`
+	DisplayName string `json:"display_name"`
+}
+
+type LoginRequest struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type UserResponse struct {
+	ID          int    `json:"id"`
+	Login       string `json:"login"`
+	DisplayName string `json:"display_name"`
+}
+
+func userToResponse(user domains.User) UserResponse {
+
+	return UserResponse{
+		ID:          user.ID,
+		Login:       user.Login,
+		DisplayName: user.DisplayName,
+	}
+}
