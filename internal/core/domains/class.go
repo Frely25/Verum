000bd00afa3dfcd83fe35641e1f493016ab18ec3
Frely25 <okefrely@gmail.com>
2026-09-19 12:53:42 +0300

    Расширил структуру domain

--- internal/core/domains/class.go
+++ internal/core/domains/class.go
@@ -1,7 +1,11 @@
 package domains
 
+import "time"
+
 type Class struct {
-	ID       int
-	Name     string
-	JoinCode string
+	ID        int
+	Name      string
+	JoinCode  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
