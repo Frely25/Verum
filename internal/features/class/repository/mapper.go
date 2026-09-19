8809e435972da93e58a4a76b936b5d074684b685
Frely25 <okefrely@gmail.com>
2026-09-19 13:06:29 +0300

    Расширил mapper под новый domain и modle

--- internal/features/class/repository/mapper.go
+++ internal/features/class/repository/mapper.go
@@ -4,16 +4,20 @@ import "github.com/Frely25/Verum/internal/core/domains"
 
 func toDomain(model classModel) domains.Class {
 	return domains.Class{
-		ID:       model.ID,
-		Name:     model.Name,
-		JoinCode: model.JoinCode,
+		ID:        model.ID,
+		Name:      model.Name,
+		JoinCode:  model.JoinCode,
+		CreatedAt: model.CreatedAt,
+		UpdatedAt: model.UpdatedAt,
 	}
 }
 
 func fromDomain(domain domains.Class) classModel {
 	return classModel{
-		ID:       domain.ID,
-		Name:     domain.Name,
-		JoinCode: domain.JoinCode,
+		ID:        domain.ID,
+		Name:      domain.Name,
+		JoinCode:  domain.JoinCode,
+		CreatedAt: domain.CreatedAt,
+		UpdatedAt: domain.UpdatedAt,
 	}
 }
